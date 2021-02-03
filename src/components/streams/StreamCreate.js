ccb672de1bee703539de2d4dc8fd4dc10ff36365
Sean OReilly <soreilly424@gmail.com>
2021-02-03 11:20:49 -0500

    redux form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return (
-    <div>StreamCreate</div>
-  )
+class StreamCreate extends React.Component {
+  renderError = ({ touched, error }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+
+  renderForm = ({ input, label, meta }) => {
+    const fieldClass = `field ${meta.error && meta.touched ? 'error' : ''}`
+    return (
+      <div className={fieldClass}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)} className="ui form error">
+        <Field name="title" component={this.renderForm} label="Title"></Field>
+        <Field
+          name="description"
+          component={this.renderForm}
+          label="Description"
+        ></Field>
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
 }
 
-export default StreamCreate;
+const validate = (values) => {
+  const errors = {};
+  if (!values.title) {
+    errors.title = "Title is required...";
+  }
+
+  if (!values.description) {
+    errors.description = "Description is required...";
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: "StreamCreate",
+  validate,
+})(StreamCreate);
--- src/index.js
+++ src/index.js
@@ -12,13 +12,11 @@ import reducers from './reducers'
 const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
-  <React.StrictMode>
     <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
+    </BrowserRouter>,
   document.getElementById("root")
 );
 
